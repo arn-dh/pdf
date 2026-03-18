42f6b839dcf14109d8103402fcee4122a5767ff5
Arnaud D <arno010@hotmail.com>
2026-03-18 12:33:36 +0000

    Update CSV URLs to include gid and single parameters

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const SITE_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?output=csv";
-const DOCS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?output=csv";
+const SITE_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?gid=1972292236&single=true&output=csv";
+const DOCS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?gid=0&single=true&output=csv";
