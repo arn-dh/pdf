7e09dfe490be3ffd1081f8d9abd566659068aa3f
Arnaud D <arno010@hotmail.com>
2026-03-18 11:47:27 +0000

    Add CSV URLs for site and docs configuration

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SITE_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?output=csv";
+const DOCS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBRNywCUXtuErdWe-NruEud3OcOLucMof3oO8taZU5n9sETW-gKJMi3sv9w0GmRwiVf7S4Gy23Znuv/pub?output=csv";
